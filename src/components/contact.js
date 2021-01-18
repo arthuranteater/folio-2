364baa02a1f5c1d1ecd4f54cbed23b5a4fd64e75
Hunt Applegate <huntapplegate@gmail.com>
2021-01-18 12:47:29 -0600

    added captcha keys

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,15 +11,15 @@ import Recaptcha from "react-google-recaptcha"
 
 //recaptcha key check
 
-// const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
-// if (typeof RECAPTCHA_KEY === 'undefined') {
-//   throw new Error(`
-//   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined!
-//   You probably forget to set it in your Netlify build environment variables.
-//   Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
-//   Note this demo is specifically for Recaptcha v2
-//   `)
-// }
+const RECAPTCHA_KEY = process.env.GATSBY_RECAPTCHA_KEY
+if (typeof RECAPTCHA_KEY === "undefined") {
+  throw new Error(`
+  Env var GATSBY_RECAPTCHA_KEY is undefined!
+  You probably forget to set it in your Netlify build environment variables.
+  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+  Note this demo is specifically for Recaptcha v2
+  `)
+}
 
 // encoding form data
 
